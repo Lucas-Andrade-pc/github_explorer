475169153a426224bffcc60f0c80925c0bcba931
Lucas-Andrade-Pc <lucas.andrade.96@hotmail.com>
2021-06-28 18:09:38 -0300

    adicionando ao git

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,10 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, FormEvent, useEffect } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/logotipo.svg';
 import api from '../../services/api';
 import Repository from '../Repository';
-import { Title, Form, Repositories } from './styles';
+import { Title, Form, Repositories, Error } from './styles';
 
 interface Repository {
     // eslint-disable-next-line camelcase
@@ -19,26 +20,42 @@ interface Repository {
 
 const Dashboard: React.FC = () => {
     const [newRepo, setNewRepo] = useState('');
+    const [inputError, setInputError] = useState('');
     const [repositories, setRepositories] = useState<Repository[]>([]);
+
+    useEffect(() => {
+        localStorage.setItem(
+            '@GithubExplorer:repositories',
+            JSON.stringify(repositories),
+        );
+    }, [repositories]);
 
     async function handleAddRepository(
         event: FormEvent<HTMLFormElement>,
     ): Promise<void> {
         event.preventDefault();
+        if (!newRepo) {
+            setInputError('Digite o auto/nome do repositorio');
+            return;
+        }
+        try {
+            const response = await api.get<Repository>(`repos/${newRepo}`);
 
-        const response = await api.get<Repository>(`repos/${newRepo}`);
+            const repository = response.data;
 
-        const repository = response.data;
-
-        setRepositories([...repositories, repository]);
-        setNewRepo('');
+            setRepositories([...repositories, repository]);
+            setNewRepo('');
+            setInputError('');
+        } catch (err) {
+            setInputError('erro na busca por esse repositorio');
+        }
     }
 
     return (
         <>
             <img src={logo} alt="Github_Explorer" />
             <Title> Explore repositórios no GitHub </Title>
-            <Form onSubmit={handleAddRepository}>
+            <Form hasError={!!inputError} onSubmit={handleAddRepository}>
                 <input
                     value={newRepo}
                     onChange={(e) => setNewRepo(e.target.value)}
@@ -46,9 +63,14 @@ const Dashboard: React.FC = () => {
                 />
                 <button type="submit"> Pesquisar </button>
             </Form>
+
+            {inputError && <Error>{inputError}</Error>}
             <Repositories>
                 {repositories.map((repository) => (
-                    <a key={repository.full_name} href="teste">
+                    <Link
+                        key={repository.full_name}
+                        to={`/repositories/${repository.full_name}`}
+                    >
                         <img
                             src={repository.owner.avatar_url}
                             alt={repository.owner.login}
@@ -58,7 +80,7 @@ const Dashboard: React.FC = () => {
                             <p>{repository.description}</p>
                         </div>
                         <FiChevronRight size={20} />
-                    </a>
+                    </Link>
                 ))}
             </Repositories>
         </>
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,73 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Header, RepositoryInfo, Issues } from './styles';
+import logo from '../../assets/logotipo.svg';
+import api from '../../services/api';
+
+interface RepositoryParams {
+    repository: string;
+}
 
 const Repository: React.FC = () => {
-    return <h1> Amem </h1>;
+    const { params } = useRouteMatch<RepositoryParams>();
+
+    useEffect(() => {
+        api.get(`repos/$${params.repository}`).then((response) => {
+            console.log(response.data);
+        });
+        api.get(`repos/$${params.repository}/issues`).then((response) => {
+            console.log(response.data);
+        });
+    });
+
+    return (
+        <>
+            <Header>
+                <img src={logo} alt="github explorer" />
+                <Link to="/">
+                    <FiChevronLeft size={16} />
+                    Voltar
+                </Link>
+            </Header>
+
+            <RepositoryInfo>
+                <header>
+                    <img
+                        src="https://avatars.githubusercontent.com/u/69631?v=4"
+                        alt="facebook"
+                    />
+                    <div>
+                        <strong>facebook/react</strong>
+                        <p>descricao</p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong>1800</strong>
+                        <strong>Starts</strong>
+                    </li>
+                    <li>
+                        <strong>48</strong>
+                        <strong>Forks</strong>
+                    </li>
+                    <li>
+                        <strong>67</strong>
+                        <strong>Issues abertas</strong>
+                    </li>
+                </ul>
+            </RepositoryInfo>
+            <Issues>
+                <Link to="dale">
+                    <div>
+                        <strong> repository.full_name </strong>
+                        <p>repository.description</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </Link>
+            </Issues>
+        </>
+    );
 };
 
 export default Repository;
